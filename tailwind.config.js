a31376075ec0879d3ae002cef5bdc247f66407f0
SamuvelRaja <josephsamuvel001@gmail.com>
2025-09-02 18:09:52 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: { extend: {} },
+  plugins: [],
+};
+
